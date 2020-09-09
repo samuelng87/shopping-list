eddc9a4a61e1e26928860cc36006e2c056e6a114
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-09-09 23:00:06 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul')
+var li = document.getElementsByTagName('li')
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement () {
+	var li = document.createElement('li')
+	li.appendChild(document.createTextNode(input.value))
+	ul.appendChild(li)
+	input.value = '';
+
+//Done 
+var button = document.createElement('button')
+button.appendChild(document.createTextNode('Done!'))
+li.appendChild(button)
+button.onclick = underlineParent
+
+//delete
+var button = document.createElement('button')
+button.appendChild(document.createTextNode('Delete'))
+li.appendChild(button)
+button.onclick = removeParent
+ 
+
+}
+
+//Done with line through 
+li.onclick = function(event) {
+	var target = event.target;
+target.classList.toggle("done"); 
+}
+
+
+//Done
+function underlineParent () {
+	event.target.parentNode.classList.toggle('done')
+}
+
+//Delete
+function removeParent (dlt) {
+	dlt.target.parentNode.remove()
+}
+
+// click button
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement ()
+	}
+
+}
+// enter button
+function addListAfterKeypress (event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement()
+}
+}
+
+
+var button =document.createElement("button");
+
+	
+
+button.addEventListener('click', addListAfterClick)
+
+input.addEventListener('keypress', addListAfterKeypress)
+
+
+var h1 = document.querySelector('h1')
+
+h1.className = 'coolTitle'
