612dc70f1b502c6e471fd0e24c92fc3e6bcb3d44
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-09-12 16:10:58 +1000

    hold on button

--- script.js
+++ script.js
@@ -3,6 +3,94 @@ var input = document.getElementById('userinput');
 var ul = document.querySelector('ul')
 var li = document.getElementsByTagName('li')
 
+
+function inputLength () {
+return input.value.length;
+}
+
+function createListElement () {
+	var li = document.createElement('li');
+		li.appendChild(document.createTextNode(input.value))
+		ul.appendChild(li)
+		input.value = ''
+
+
+	var button = document.createElement('button')	
+	button.appendChild(document.createTextNode('Done~'))
+	li.appendChild(button)
+button.onclick = underLinewords
+
+var button = document.createElement('button')	
+button.appendChild(document.createTextNode('Delete'))
+li.appendChild(button)
+button.onclick = removeList
+
+var button = document.createElement('button')	
+button.appendChild(document.createTextNode('hold'))
+li.appendChild(button)
+button.onclick = holdList
+
+}
+
+function holdList () {
+	event.target.parentNode.classList.add("holdon")
+}
+
+
+function underLinewords () {
+	event.target.parentNode.classList.toggle('done')
+}
+
+
+function removeList (clear) {
+	clear.target.parentNode.remove()
+}
+
+
+function addListAfterClick () {
+	if (inputLength() > 0) {
+		createListElement();
+		}
+}
+
+function addListAfterKeypress (event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+	createListElement();
+	}
+}
+
+
+
+button.addEventListener('click', addListAfterClick)
+input.addEventListener('keypress',addListAfterKeypress)
+
+ 
+
+
+
+var h1 = document.querySelector('h1')
+
+h1.className = 'coolTitle'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul')
+var li = document.getElementsByTagName('li')
+
 function inputLength() {
 	return input.value.length;
 }
@@ -72,3 +160,4 @@ input.addEventListener('keypress', addListAfterKeypress)
 var h1 = document.querySelector('h1')
 
 h1.className = 'coolTitle'
+ */
